45ae5c9f8b9b710c3524fac29abbbbabe658d613
Andrew Kibiwott <kibiandrew@gmail.com>
2022-12-07 23:52:12 +0300

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let input=document.getElementById('new-task-description').value;
+    const li =document.createElement("li");
+    li.textContent=input;
+    document.querySelector('#tasks').appendChild(li);
+    document.querySelector('form').reset()
+
+  })
 });
